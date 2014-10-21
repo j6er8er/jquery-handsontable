9600459dd7d4c7f1e3e8f16845f99f81e78d8e05
Jeff Gerber <jgerber@apptio.com>
2014-10-21 10:26:53 -0700

    modified _generate to skip values for keys where the value === undefined

--- dist/jquery.handsontable.full.js
+++ dist/jquery.handsontable.full.js
@@ -7394,6 +7394,7 @@ var jsonpatch;
         for (var t = 0; t < newKeys.length; t++) {
             var key = newKeys[t];
             if (!mirror.hasOwnProperty(key)) {
+		if(obj[key] === undefined) continue;
                 patches.push({ op: "add", path: path + "/" + escapePathComponent(key), value: obj[key] });
                 mirror[key] = JSON.parse(JSON.stringify(obj[key]));
             }
